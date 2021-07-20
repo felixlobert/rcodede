d8c7b3ebf0d322b2b5236d40d9d9940cf6e35db6
felixlobert <felixlobert@freenet.de>
2021-07-20 12:23:19 +0000

    fix coord bug when counting geometries covered by the scenes / enable to search for all available satellites at the same time

--- R/getScenes.R
+++ R/getScenes.R
@@ -6,7 +6,7 @@
 #' @param bufferDist Buffer around the AOI in meters. Defaults to 0.
 #' @param startDate Starting date for the query of format "YYYY-MM-DD".
 #' @param endDate End date for query.
-#' @param satellite Satellite to query from. One of "Sentinel1", "Sentinel2" etc.
+#' @param satellite Satellite to query from. One of "Sentinel1", "Sentinel2" etc. If none is chosen, all available results are returned.
 #' @param productType Product type to query. One of "SLC", "GRD", "CARD-INF6", "CARD-BS", "CARD-BS-MC", "L3-WASP"... Only considered if set.
 #' @param sensorMode Sensor mode for Sentinel-1. One of "IW", "EW", ... Only considered if set.
 #' @param orbitDirection Direction of satellite orbit. One of "ascending" or "descending". Only considered if set.
@@ -37,7 +37,7 @@ getScenes <-
            bufferDist = NULL,
            startDate = NULL,
            endDate = NULL,
-           satellite,
+           satellite = NULL,
            productType = NULL,
            sensorMode = NULL,
            orbitDirection = NULL,
@@ -83,7 +83,10 @@ getScenes <-
       gsub(" ", "+", .)
 
     url = paste0(
-      "https://finder.code-de.org/resto/api/collections/", satellite, "/search.json?maxRecords=1000&location=all&sortParam=startDate&sortOrder=descending&status=all&dataset=ESA-DATASET",
+      "https://finder.code-de.org/resto/api/collections/",
+      if (!is.null(satellite))
+        paste0(satellite,"/"),
+      "search.json?maxRecords=1000&location=all&sortParam=startDate&sortOrder=descending&status=all&dataset=ESA-DATASET",
       if (!is.null(orbitDirection))
         paste0("&orbitDirection=", orbitDirection),
       if (!is.null(startDate))
@@ -120,22 +123,23 @@ getScenes <-
       dplyr::mutate(wkt = gsub(";", " ", wkt)) %>%
       dplyr::mutate(wkt = paste0("POLYGON (( ", wkt, " ))")) %>%
       dplyr::select(
-        date = startDate,
-        platform,
-        relativeOrbitNumber,
-        orbitDirection,
-        centroidLat = lat,
-        centroidLon = lon,
-        productPath = productIdentifier,
-        footprint = wkt
-      ) %>%
+          date = startDate,
+          platform,
+          relativeOrbitNumber,
+          orbitDirection,
+          productType,
+          centroidLat = lat,
+          centroidLon = lon,
+          productPath = productIdentifier,
+          footprint = wkt
+        ) %>%
       tidyr::separate(date, c("date", NA), sep = "T") %>%
       dplyr::mutate(date = as.Date(date)) %>%
       dplyr::arrange(date) %>%
       sf::st_as_sf(wkt = "footprint") %>%
       sf::`st_crs<-`(sf::st_crs(4326)) %>%
-      dplyr::mutate(numberAoiGeoms = sf::st_contains_properly(., aoi.processed) %>% lengths) %>%
-      dplyr::select(date, platform, orbitDirection, relativeOrbitNumber, centroidLat, centroidLon, productPath, numberAoiGeoms, footprint) %>%
+      dplyr::mutate(numberAoiGeoms = sf::st_contains_properly(sf::st_transform(., aoi.epsg), sf::st_transform(aoi.processed, aoi.epsg)) %>% lengths) %>%
+      dplyr::select(date, platform, productType, orbitDirection, relativeOrbitNumber, centroidLat, centroidLon, productPath, numberAoiGeoms, footprint) %>%
       dplyr::arrange(date, relativeOrbitNumber) %>%
       dplyr::mutate(date = as.Date(date))
 
